4f1da0e8e86b197c8dfde74a59b14dbbca849635
LalaFaulk <154087476+LalaFaulk@users.noreply.github.com>
2025-11-29 23:24:41 -0500

    Add method to the DiscountService file.

--- src/com/shashi/service/DiscountService.java
+++ src/com/shashi/service/DiscountService.java
@@ -3,5 +3,7 @@ package com.shashi.service;
 import com.shashi.beans.DiscountBean;
 
 public interface DiscountService {
+	
+	public String updateProductPrice(String prodId, double prodDiscount);
 
 }
