3b7c2666dcf24a6cc66da2a21cc79cd318f1c62d
LalaFaulk <154087476+LalaFaulk@users.noreply.github.com>
2025-11-30 01:02:24 -0500

    Edited parameters for two methods.

--- src/com/shashi/service/DiscountService.java
+++ src/com/shashi/service/DiscountService.java
@@ -6,9 +6,9 @@ public interface DiscountService {
 	
 	public String calculateDiscount(String prodId, double prodDiscount, double regPrice);
 
-	public String addProduct(String prodId);
+	public String addProduct(String prodId, double prodDiscount, double discountPrice);
 	
-	public String removeProduct(String prodId);
+	public String removeProduct(String prodId, double prodDiscount);
 	
 	public String updateProductPrice(String prodId, double discountPrice);
 	
