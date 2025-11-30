59d54d89960e45568f9f8b021c75114dc34c0389
LalaFaulk <154087476+LalaFaulk@users.noreply.github.com>
2025-11-30 00:05:33 -0500

    Updated method in DiscountService.

--- src/com/shashi/service/DiscountService.java
+++ src/com/shashi/service/DiscountService.java
@@ -4,6 +4,6 @@ import com.shashi.beans.DiscountBean;
 
 public interface DiscountService {
 	
-	public String updateProductPrice(String prodId, double prodDiscount);
+	public String calculateDiscount(String prodId, double prodDiscount, double prodPrice);
 
 }
