d2943d0e9fdd8bdc47d31b1db00c3a80a793e79b
LalaFaulk <154087476+LalaFaulk@users.noreply.github.com>
2025-12-01 23:38:04 -0500

    Edited to add field and methods that match table.

--- src/com/shashi/beans/DiscountBean.java
+++ src/com/shashi/beans/DiscountBean.java
@@ -7,15 +7,17 @@ public class DiscountBean implements Serializable {
 	private String prodId;
 	private String discountType;
 	private double discountPercent;
+	private double regularPrice;
 	
 	public DiscountBean() {
 	}
 	
-	public DiscountBean(String prodId, String discountType, double discountPercent) {
+	public DiscountBean(String prodId, String discountType, double discountPercent, double regularPrice) {
 		super();
 		this.prodId = prodId;
 		this.discountType = discountType;
 		this.discountPercent = discountPercent;
+		this.regularPrice = regularPrice;
 	}
 	
 	public String getProdId() {
@@ -41,6 +43,14 @@ public class DiscountBean implements Serializable {
 	
 	public void setDiscountPercent(double discountPercent) {
 		this.discountPercent = discountPercent;
+	}
+	
+	public double getRegularPrice() {
+		return regularPrice;
+	}
+	
+	public void setRegularPrice(double regularPrice) {
+		this.regularPrice = regularPrice;
 	}
 
 }
